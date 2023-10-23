18d26d24c203d833285f39d7cf5cf8a140a3876c
NhatKhai352528 <nhatkhai352528@gmail.com>
2023-10-23 22:03:36 +0700

    init Exercise3

--- STM32/Core/Inc/main.h
+++ STM32/Core/Inc/main.h
@@ -57,6 +57,50 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define BUTTON_0_Pin GPIO_PIN_0
+#define BUTTON_0_GPIO_Port GPIOB
+#define BUTTON_1_Pin GPIO_PIN_1
+#define BUTTON_1_GPIO_Port GPIOB
+#define BUTTON_2_Pin GPIO_PIN_2
+#define BUTTON_2_GPIO_Port GPIOB
+#define TRAFFIC0_GREEN_Pin GPIO_PIN_10
+#define TRAFFIC0_GREEN_GPIO_Port GPIOB
+#define TRAFFIC1_RED_Pin GPIO_PIN_11
+#define TRAFFIC1_RED_GPIO_Port GPIOB
+#define TRAFFIC1_AMBER_Pin GPIO_PIN_12
+#define TRAFFIC1_AMBER_GPIO_Port GPIOB
+#define TRAFFIC1_GREEN_Pin GPIO_PIN_13
+#define TRAFFIC1_GREEN_GPIO_Port GPIOB
+#define LED7SEG_A_Pin GPIO_PIN_8
+#define LED7SEG_A_GPIO_Port GPIOA
+#define LED7SEG_B_Pin GPIO_PIN_9
+#define LED7SEG_B_GPIO_Port GPIOA
+#define LED7SEG_C_Pin GPIO_PIN_10
+#define LED7SEG_C_GPIO_Port GPIOA
+#define LED7SEG_D_Pin GPIO_PIN_11
+#define LED7SEG_D_GPIO_Port GPIOA
+#define LED7SEG_E_Pin GPIO_PIN_12
+#define LED7SEG_E_GPIO_Port GPIOA
+#define LED7SEG_F_Pin GPIO_PIN_13
+#define LED7SEG_F_GPIO_Port GPIOA
+#define LED7SEG_G_Pin GPIO_PIN_14
+#define LED7SEG_G_GPIO_Port GPIOA
+#define LED7SEG_DP_Pin GPIO_PIN_15
+#define LED7SEG_DP_GPIO_Port GPIOA
+#define BUTTON_3_Pin GPIO_PIN_3
+#define BUTTON_3_GPIO_Port GPIOB
+#define LED7SEG_0_Pin GPIO_PIN_4
+#define LED7SEG_0_GPIO_Port GPIOB
+#define LED7SEG_1_Pin GPIO_PIN_5
+#define LED7SEG_1_GPIO_Port GPIOB
+#define LED7SEG_2_Pin GPIO_PIN_6
+#define LED7SEG_2_GPIO_Port GPIOB
+#define LED7SEG_3_Pin GPIO_PIN_7
+#define LED7SEG_3_GPIO_Port GPIOB
+#define TRAFFIC0_RED_Pin GPIO_PIN_8
+#define TRAFFIC0_RED_GPIO_Port GPIOB
+#define TRAFFIC0_AMBER_Pin GPIO_PIN_9
+#define TRAFFIC0_AMBER_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
