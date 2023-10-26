49aaa723e2b08b77d6318956705a70c6628d76f1
NhatKhai352528 <nhatkhai352528@gmail.com>
2023-10-26 22:55:43 +0700

    init Exercise9

--- STM32/Core/Src/processing_fsm.c
+++ STM32/Core/Src/processing_fsm.c
@@ -207,6 +207,21 @@ void fsmProcessing(void) {
 		if (buttonPressed(0)) {
 			fsmReInit(FSM_NORMAL);
 		}
+		if (buttonPressed(1)) {
+			led7segNumbers[1] ++;
+			if (led7segNumbers[1] >= pow(10, LED7SEG_DIGIT_NUMBER)) {
+				led7segNumbers[1] = 0;
+			}
+		}
+		if (buttonPressed(2)) {
+			led7segNumbers[1] --;
+			if (led7segNumbers[1] < 0) {
+				led7segNumbers[1] = pow(10, LED7SEG_DIGIT_NUMBER) - 1;
+			}
+		}
+		if (buttonPressed(3)) {
+			trafficGreenDuration = led7segNumbers[1];
+		}
 		break;
 	default:
 		break;
